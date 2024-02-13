19db77dcda27c3ca0e451d3b3f2cc2fd8373ba86
Ajay Pulapa <122472441+apulapa@users.noreply.github.com>
2024-02-13 22:51:24 +0530

    Add files via upload

---
+++ src/main/java/com/demo/SpringAppServiceDeployApplication.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAppServiceDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppServiceDeployApplication.class, args);
+	}
+	
+	@GetMapping("/get")
+	public String get()
+	{
+		return "Application Deployed in the Azure App Service";
+	}
+
+}
